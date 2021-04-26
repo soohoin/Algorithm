97f5703b279e33ece0589e025c6ed5b23f42c914
soohoin <soohoin12@naver.com>
2021-04-26 21:38:10 +0900

    폴더변경

--- src/enji/baekjoon/App.java
+++ src/enji/baekjoon/App.java
@@ -1,5 +1,6 @@
+package enji.baekjoon;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
---
+++ src/yeongcheol/baekjoon/App.java
@@ -0,0 +1,6 @@
+package yeongcheol.baekjoon;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
