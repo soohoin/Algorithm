61b4dcf66fe00d7b3a6a166204462b052f1fa5e3
soohoin <soohoin12@naver.com>
2021-07-18 01:04:00 +0900

    백준(11654번) - 아스키 코드

---
+++ src/yeongcheol/baekjoon/level07/Level07_01.java
@@ -0,0 +1,13 @@
+package level07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Level07_01 {
+    // 11654번 - 아스키 코드
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.read());
+    }
+}
