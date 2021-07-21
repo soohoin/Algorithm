1e5979266ec571070ef977c0b70648041898d233
soohoin <soohoin12@naver.com>
2021-07-22 07:14:19 +0900

    백준 1157번 - 단어공부

--- src/yeongcheol/baekjoon/level07/Level07_05.java
+++ src/yeongcheol/baekjoon/level07/Level07_05.java
@@ -3,17 +3,28 @@ package level07;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Level07_05 {
     // 1157 - 단어공부
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String str = br.readLine();
-        for(int i=0; i<str.length(); i++) {
-            
+        int [] charCntArray = new int[26];
+        char[] charArray = br.readLine().toCharArray();
+        int minus, maxNum, maxIndex = 0;
+        
+        for(int i=0; i<charArray.length; i++) {
+            minus = charArray[i] > 90 ? 97 : 65;
+            charCntArray[charArray[i] - minus]++;
         }
-        System.out.println(sb);
+        maxNum = charCntArray[maxIndex];
+        for(int i=1; i<charCntArray.length; i++) {
+            if(maxNum < charCntArray[i]) {
+                maxNum = charCntArray[i];
+                maxIndex = i;
+            } else if(maxNum == charCntArray[i]) {
+                maxIndex = -2;
+            }
+        }
+        System.out.println((char)(maxIndex + 65));
     }
 }
