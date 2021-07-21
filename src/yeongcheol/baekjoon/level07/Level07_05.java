11567cf7d2ad94dac315fefd2830af4f607290b4
soohoin <soohoin12@naver.com>
2021-07-21 23:36:44 +0900

    백준 1157 - 단어공부

---
+++ src/yeongcheol/baekjoon/level07/Level07_05.java
@@ -0,0 +1,19 @@
+package level07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Level07_05 {
+    // 1157 - 단어공부
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        for(int i=0; i<str.length(); i++) {
+            
+        }
+        System.out.println(sb);
+    }
+}
