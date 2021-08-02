8028d6654eefb52213eede1f2a51ccb83092e3b6
soohoin <soohoin12@naver.com>
2021-08-02 20:16:01 +0900

    백준 2292번 벌집

---
+++ src/yeongcheol/baekjoon/level08/level08_02.java
@@ -0,0 +1,21 @@
+package level08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class level08_02 {
+    // 2292번 벌집
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        int distance = 1;
+        int checkNumber = 1;
+        while(true) {
+            if(n <= checkNumber) break;
+            distance++;
+            checkNumber += (distance-1) * 6;
+        }
+        System.out.println(distance);
+    }
+}
